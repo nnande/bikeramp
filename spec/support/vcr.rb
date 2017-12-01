0573293d7e9b4a394e867eb16e52efb297c964d8
Nathaniel Nande <n.t.nande@gmail.com>
2017-12-01 23:43:03 +0100

    Add VCR gem config file

---
+++ spec/support/vcr.rb
@@ -0,0 +1,8 @@
+VCR.configure do |c|
+  c.allow_http_connections_when_no_cassette = false
+  c.cassette_library_dir  = Rails.root.join('spec', 'vcr')
+  c.hook_into :webmock
+  c.ignore_localhost = true
+  c.configure_rspec_metadata!
+  c.default_cassette_options = { record: :new_episodes }
+end
