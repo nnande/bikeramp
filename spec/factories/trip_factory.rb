39102d7099b99f5a92ec91f6fa5fc562fbfedc0d
Nathaniel Nande <n.t.nande@gmail.com>
2017-12-01 11:04:55 +0100

    Add trip factory

---
+++ spec/factories/trip_factory.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+	factory :trip do
+		price 100
+		date { Time.current }
+		distance 15
+		
+		start_address { build(:address) }
+		destination_address { build(:address) }
+	end
+end
+
