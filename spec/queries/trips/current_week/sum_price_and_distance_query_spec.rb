683b997ef881c06928d213c71b9ca16b79de94b6
Nathaniel Nande <n.t.nande@gmail.com>
2017-12-02 02:18:25 +0100

    Add Trips::CurrentWeek::SumPriceAndDistance Query class

---
+++ spec/queries/trips/current_week/sum_price_and_distance_query_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Trips::CurrentWeek::SumPriceAndDistanceQuery, type: :query do
+	subject { described_class.new }
+
+	describe '#all', :vcr do
+		let!(:current_week_trip) { create(:trip) }
+		let!(:last_week_trip) { create(:trip, date: Time.current - 1.week) }
+		
+		it 'returns price and distance sum from current week' do
+			result = subject.all[0]
+
+			expect(result.total_price).to eq(current_week_trip.price)
+			expect(result.total_distance).to eq(current_week_trip.distance)
+		end
+	end
+end
