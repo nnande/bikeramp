2fd857a7479148bd9eb63620b76a83710852a4bf
Nathaniel Nande <n.t.nande@gmail.com>
2017-12-03 15:22:53 +0100

    WeeklyStartsPresenter class

---
+++ spec/presenters/weekly_stats_presenter_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe WeeklyStatsPresenter,:vcr, type: :presenter do
+	subject { described_class.new(object) }
+
+	let!(:trip) { create(:trip) }
+	let(:object) { res = Trips::CurrentWeek::SumPriceAndDistanceQuery.new.all; res[0] }
+	let(:result) { { total_distance: '0km', total_price: '100.0PLN' } }
+
+	describe '#as_json' do
+		it 'returns json formatted weekly stats' do
+			expect(subject.as_json).to eq(result)
+		end
+	end
+end
